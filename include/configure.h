5f478e6d6afd4a113bf18247b7c06f15272396b6
fthoele <florian.thoele@mat.ethz.ch>
2016-09-13 17:10:39 +0200

    modified gitignoe

--- include/configure.h
+++ include/configure.h
@@ -14,34 +14,34 @@
 #define  __HAVE_CONFIG_INFO
 
 #define   __CONF_HOST           "@host@"
-#define   __CONF_ARCH           "x86_64"
+#define   __CONF_ARCH           "mac686"
 
-#define   __CONF_CC             "mpicc"
+#define   __CONF_CC             "cc"
 #define   __CONF_CFLAGS         "-O3"
-#define   __CONF_DFLAGS         " -D__OPENMP -D__INTEL -D__DFTI"
+#define   __CONF_DFLAGS         " -D__GFORTRAN -D__STD_F95 -D__FFTW"
 #define   __CONF_CPP            "cpp"
 #define   __CONF_CPPFLAGS       "-P -traditional"
 
-#define   __CONF_F90            "ifort"
+#define   __CONF_F90            "gfortran"
 #define   __CONF_MPIF90         "mpif90"
-#define   __CONF_F90FLAGS       "$(FFLAGS) -nomodule -qopenmp"
-#define   __CONF_F77            "ifort"
-#define   __CONF_FFLAGS         "-O2 -assume byterecl -g -traceback -qopenmp"
-#define   __CONF_FFLAGS_NOOPT   "-O0 -assume byterecl -g -traceback"
-#define   __CONF_PRE_FDFLAGS    "-fpp "
+#define   __CONF_F90FLAGS       "$(FFLAGS) -x f95-cpp-input"
+#define   __CONF_F77            "gfortran"
+#define   __CONF_FFLAGS         "-O3 -g"
+#define   __CONF_FFLAGS_NOOPT   "-O0 -g"
+#define   __CONF_PRE_FDFLAGS    ""
 #define   __CONF_FDFLAGS        "$(DFLAGS) $(MANUAL_DFLAGS)"
 
 #define   __CONF_LD             "mpif90"
-#define   __CONF_LDFLAGS        "-static-intel  -qopenmp"
+#define   __CONF_LDFLAGS        "-g -pthread"
 #define   __CONF_IMOD           "-I"
 
-#define   __CONF_BLAS_LIBS      "  -lmkl_intel_lp64  -lmkl_intel_thread -lmkl_core"
-#define   __CONF_LAPACK_LIBS    ""
+#define   __CONF_BLAS_LIBS      " -lblas "
+#define   __CONF_LAPACK_LIBS    " -llapack  -lblas "
 #define   __CONF_FFT_LIBS       ""
 #define   __CONF_MASS_LIBS      ""
 
 #define   __CONF_AR             "ar"
 #define   __CONF_ARFLAGS        "ruv"
-#define   __CONF_RANLIB         "ranlib"
+#define   __CONF_RANLIB         "ranlib -c"
 
 
